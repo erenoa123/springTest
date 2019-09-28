53138b1d36cd88a19e9a25b3123a9eb8fa30b1a4
kurihara <kurihara@DESKTOP-1DRN73P>
2019-09-29 05:09:49 +0900

    bootstrap 連携

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -9,22 +9,21 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HeloController {
 
-//	@RequestMapping("/")
-//	public String index(Model model) {
-//		 // 横軸
-//        // ラベルです。String型の配列に、適当に値を入れておきます。
-//        String label[] = {"a","b","c","d","e","f","g"};
-//
-//        // 縦軸
-//        // 具体的な値です。int型で、この値も適当です。
-//        int point[] = {5,3,7,1,8,3,4,};
-//
-//        // Modelに格納します。ビュー側でグラフ用の配列を受け取れるようにしておきます。
-//        model.addAttribute("label",label);
-//        model.addAttribute("point",point);
-//
-//		return "index";
-//	}
+	@RequestMapping("/home")
+	public String Home() {
+		return "starter_test";
+	}
+
+	@RequestMapping("/regist/player")
+	public String RegistPlayer() {
+		return "starter_test_form";
+	}
+
+	@RequestMapping("/regist/training")
+	public String home() {
+		return "starter_test_form_training";
+	}
+
 
 	@RequestMapping(value = "/", method=RequestMethod.GET)
 	public ModelAndView index2(ModelAndView mav) {
